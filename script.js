85109c1443553bd6eb5917070703ca434357b8a4
Manu Sharma <manusharma2462@gmail.com>
2025-09-19 17:32:44 +0530

    add the functionality to edit your multiple post

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ let form = document.getElementById("form");
 let input = document.getElementById("input");
 let msg = document.getElementById("msg");
 let post = document.getElementById("post");
+
 form.addEventListener("submit", (e) => {
   e.preventDefault();
   console.log("clicked");
@@ -23,13 +24,28 @@ let data = {};
 function acceptdata() {
   data["text"] = input.value;
   console.log(data);
-  createPost();
+  if (editingId) {
+    updatePost();
+  } else {
+    createPost();
+  }
 }
+
+function updatePost() {
+  const currentPost = document.getElementById(editingId);
+  currentPost.textContent = data.text;
+  editingId = null;
+  editingContent = null;
+}
+
+let currentIndex = 0;
+let editingId = null;
+let editingContent = null;
 
 function createPost() {
   post.innerHTML += `
     <div>
-      <p>${data.text}</p>
+      <p id=post-${currentIndex++}>${data.text}</p>
       <span class="options">
         <i onClick="editPost(this)" class="fas fa-edit"></i>
         <i onClick="deletePost(this)" class="fas fa-trash-alt"></i>
@@ -41,13 +57,17 @@ function createPost() {
 let deletePost = (e) => {
   e.parentElement.parentElement.remove();
 };
-
 let editPost = (e) => {
-  let currentText = e.parentElement.previousElementSibling.innerHTML;
-
-  let newText = prompt("Edit your post:");
-
-  if (newText !== null) {
-    e.parentElement.previousElementSibling.innerHTML = newText;
+  if (editingId) {
+    let editingPost = document.getElementById(editingId);
+    editingPost.textContent = editingContent;
   }
+
+  let currentPost = e.parentElement.previousElementSibling;
+  const input = document.getElementById("input");
+
+  input.value = currentPost.textContent;
+  editingContent = currentPost.textContent;
+  currentPost.textContent = "Editing...";
+  editingId = currentPost.id;
 };
